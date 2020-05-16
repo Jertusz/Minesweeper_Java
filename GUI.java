312b9b52cb8fb0e39d57291f409ad6b7603d561a
Jertusz <jendziu@gmail.com>
2020-05-16 18:01:05 +0200

    moved code from mouseEvent to separate method

--- GUI.java
+++ GUI.java
@@ -18,9 +18,9 @@ public class GUI {
     }
 
     public void changeButtons() {
-        for(List<Field> line: board.getBoard()) {
-            for (Field field: line) {
-                if(field.isDiscovered()) {
+        for (List<Field> line : board.getBoard()) {
+            for (Field field : line) {
+                if (field.isDiscovered()) {
                     int[] fieldCoordinates = board.getFieldCoordinates(field);
                     buttons.get(fieldCoordinates[0]).get(fieldCoordinates[1]).setText(String.valueOf(field.getValue()));
                     setFontColor(buttons.get(fieldCoordinates[0]).get(fieldCoordinates[1]), field);
@@ -59,83 +59,85 @@ public class GUI {
         button.setBackground(Color.WHITE);
     }
 
+    public void mousePressed(MouseEvent arg0, Field field, JButton button, JFrame frame) {
+        int[] fieldCoordinates = board.getFieldCoordinates(field);
+        System.out.println(Arrays.toString(fieldCoordinates));
+        System.out.println(button.getMouseListeners()[0]);
+        boolean result;
+        if (arg0.getButton() == MouseEvent.BUTTON1) {
+            result = board.unveilField(fieldCoordinates[0], fieldCoordinates[1]);
+            if (!result && !field.isMarkedBomb()) {
+                button.setText(String.valueOf(field.getValue()));
+            } else if (field.getValue() == 9 && !field.isMarkedBomb()) {
+                int option = JOptionPane.showConfirmDialog(frame, "Przegrana! \n Czy chcesz zagrać ponownie?", "", JOptionPane.YES_NO_OPTION);
+                if (option == JOptionPane.YES_OPTION) {
+                    frame.dispose();
+                    newGame();
+                } else {
+                    frame.dispose();
+                }
+            }
+        } else if (arg0.getButton() == MouseEvent.BUTTON3) {
+            if (!field.isMarkedBomb()) {
+                result = board.markBomb(fieldCoordinates[0], fieldCoordinates[1]);
+                if (result) {
+                    Image img;
+                    try {
+                        int width = button.getWidth();
+                        int height = button.getHeight();
+                        img = ImageIO.read(getClass().getResource("resources/flag.png"));
+                        img = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+                        button.setIcon(new ImageIcon(img));
+                        button.setBackground(Color.lightGray);
+                    } catch (IOException e) {
+                        button.setText("*");
+                    }
+                }
+            } else {
+                board.unmarkBomb(fieldCoordinates[0], fieldCoordinates[1]);
+                button.setText("");
+                button.setIcon(null);
+            }
+        }
+        changeButtons();
+        if (board.winCondition()) {
+            int option = JOptionPane.showConfirmDialog(frame, "Wygrana! \n Czy chcesz zagrać ponownie?", "", JOptionPane.YES_NO_OPTION);
+            if (option == JOptionPane.YES_OPTION) {
+                frame.dispose();
+                newGame();
+            } else {
+                frame.dispose();
+            }
+        }
+    }
+
     public void showGui() {
         JFrame frame = new JFrame(gc);
         frame.setTitle("Minesweeper JS");
         JLabel status = new JLabel();
         status.setText(String.format("Bombs: %d", board.bombPositions.size()));
-        for(List<Field> line: board.getBoard()) {
+        for (List<Field> line : board.getBoard()) {
             List<JButton> temporary = new ArrayList<>();
-            for (Field field: line) {
+            for (Field field : line) {
                 JButton button = new JButton();
                 button.addMouseListener(new MouseAdapter() {
                     @Override
-                    public void mousePressed(MouseEvent arg0) {
-                        int[] fieldCoordinates = board.getFieldCoordinates(field);
-                        System.out.println(Arrays.toString(fieldCoordinates));
-                        System.out.println(button.getMouseListeners()[0]);
-                        boolean result;
-                        if (arg0.getButton() == MouseEvent.BUTTON1){
-                            result = board.unveilField(fieldCoordinates[0], fieldCoordinates[1]);
-                            if (!result && !field.isMarkedBomb()) {
-                                button.setText(String.valueOf(field.getValue()));
-                            } else if (field.getValue() == 9 && !field.isMarkedBomb()) {
-                                int option = JOptionPane.showConfirmDialog(frame, "Przegrana! \n Czy chcesz zagrać ponownie?", "", JOptionPane.YES_NO_OPTION);
-                                if(option == JOptionPane.YES_OPTION) {
-                                    frame.dispose();
-                                    newGame();
-                                } else {
-                                    frame.dispose();
-                                }
-                            }
-                        } else if (arg0.getButton() == MouseEvent.BUTTON3) {
-                            if(!field.isMarkedBomb()) {
-                                result = board.markBomb(fieldCoordinates[0], fieldCoordinates[1]);
-                                if (result) {
-                                    Image img;
-                                    try {
-                                        int width = button.getWidth();
-                                        int height = button.getHeight();
-                                        img = ImageIO.read(getClass().getResource("resources/flag.png"));
-                                        img = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
-                                        button.setIcon(new ImageIcon(img));
-                                        button.setBackground(Color.lightGray);
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                        button.setText("*");
-                                    }
-                                }
-                            } else {
-                                board.unmarkBomb(fieldCoordinates[0], fieldCoordinates[1]);
-                                button.setText("");
-                                button.setIcon(null);
-                            }
-                        }
-                        System.out.println(board);
-                        changeButtons();
-                        if(board.winCondition()) {
-                            int option = JOptionPane.showConfirmDialog(frame, "Wygrana! \n Czy chcesz zagrać ponownie?", "", JOptionPane.YES_NO_OPTION);
-                            if(option == JOptionPane.YES_OPTION) {
-                                frame.dispose();
-                                newGame();
-                            } else {
-                                frame.dispose();
-                            }
-                        }
+                    public void mousePressed(MouseEvent e) {
+                        GUI.this.mousePressed(e, field, button, frame);
                     }
                 });
-            temporary.add(button);
+                temporary.add(button);
             }
             buttons.add(temporary);
             System.out.println();
         }
-        for(List<JButton> line: buttons) {
+        for (List<JButton> line : buttons) {
             for (JButton button : line) {
                 frame.add(button);
             }
         }
 
-        frame.setSize(600,400);
+        frame.setSize(600, 400);
         JButton reset = new JButton();
         reset.setText("Reset");
         reset.addMouseListener(new MouseAdapter() {
@@ -146,8 +148,8 @@ public class GUI {
                 newGame();
             }
         });
-        frame.setLayout(new GridLayout(board.getBoard().size()+2, board.getBoard().size()-1));
-        for(int i = 0; i < board.getBoard().size(); i++) {
+        frame.setLayout(new GridLayout(board.getBoard().size() + 2, board.getBoard().size() - 1));
+        for (int i = 0; i < board.getBoard().size(); i++) {
             frame.add(new JPanel());
         }
         frame.add(status);
@@ -160,7 +162,7 @@ public class GUI {
                 frame.dispose();
             }
         });
-        for(int i = 0; i < board.getBoard().size()-3; i++) {
+        for (int i = 0; i < board.getBoard().size() - 3; i++) {
             frame.add(new JPanel());
         }
         frame.add(reset);
